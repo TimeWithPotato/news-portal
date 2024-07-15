cc19bcf969e104a6b0a9fd40388a70ad4f5c783c
TimeWithPotato <arifmainuddin18@gmail.com>
2024-07-15 11:45:56 +0600

    load the category news array done

--- js/categories.js
+++ js/categories.js
@@ -13,17 +13,40 @@ const loadCatagories = async () => {
 // display the categories
 const displayCategories = categorieses => {
 
-    console.log(categorieses);
+    // console.log(categorieses);
     const categoriesContainer = document.getElementById('categories-container');
     categorieses.forEach(categories => {
         const categoriesDiv = document.createElement('div');
         categoriesDiv.innerHTML = `
-            <li class="nav-item categories-text">
+            <li onclick="loadCategoryNews('${categories.category_id}')" class="nav-item categories-text">
                 <a class="nav-link link-primary" href="#">${categories.category_name}</a>
             </li>`;
         
         categoriesContainer.appendChild(categoriesDiv);
     });
+}
+
+//   Load the category news 
+const loadCategoryNews = async category_id => {
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+
+        const res = await fetch(url);
+        const data = await res.json();
+        displayCategoryNews(data.data);
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+// display the category news
+const displayCategoryNews = newsArray => {
+    // console.log(newsArray);
+    
+    newsArray.forEach(news => {
+        console.log(news);
+    })
 }
 
 loadCatagories();
