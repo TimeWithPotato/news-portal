8271230fc4cb07494ac1d5614b85e17171b432e2
TimeWithPotato <arifmainuddin18@gmail.com>
2024-07-15 12:28:34 +0600

    dynamically news added

--- js/categories.js
+++ js/categories.js
@@ -43,9 +43,47 @@ const loadCategoryNews = async category_id => {
 // display the category news
 const displayCategoryNews = newsArray => {
     // console.log(newsArray);
-    
+    const articleContainer = document.getElementById('article-container');
     newsArray.forEach(news => {
         console.log(news);
+
+        if (news.details.length > 200) {
+            news.details=news.details.slice(0, 200);
+        }
+        const newsContainer = document.createElement('div');
+        newsContainer.classList.add('news-container');
+        newsContainer.innerHTML = `                   
+         <img src="${news.thumbnail_url}" alt="">
+         <!-- class news-text here -->
+                    <div class="news-text">
+                        <h5>${news.title}</h5>
+                        <p>${news.details} ...</p>
+                   <!-- class news-author here -->    
+                        <div class="news-author">
+                            <div>
+                            <!-- Author image -->
+                                <div class="author-img-container">
+                                 <img class="author-img" src="${news.author.img}" alt="">
+                                 </div>
+                                 <!-- Author name and published date -->
+                                <div>
+                                    <h5>${news.author?.name ? news.author.name : 'No name found' }</h5>
+                                    <p>${news.author?.published_date ? news.author.published_date : 'No Published Date Found'}</p>
+                                </div>
+                            </div>
+                            <div>
+                                <img src="resources/carbon_view.png" alt="">
+                                <h5>${news.total_view}</h5>
+                            </div>
+                            <div class="me-3">
+                                <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#newsDetailsModal">
+                                    <img src="resources/bi_arrow-right-short.png" alt="">
+                                  </button>
+                            </div>
+                        </div>
+                    </div>`;
+        
+        articleContainer.appendChild(newsContainer);
     })
 }
 
